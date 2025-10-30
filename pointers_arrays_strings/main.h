ac98ab292e88b74f35400db48045a8cd6900c0df
Melissa <melissasad201@gmail.com>
2025-10-30 13:18:11 +0000

    1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,5 +6,3 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-
-#endif
