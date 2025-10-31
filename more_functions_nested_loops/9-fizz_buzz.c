a73461404afdd05886fb0e6861ff5c51e5be6f0c
melissa <melissasad201@gmail.com>
2025-10-31 14:41:06 +0000

    str

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
  * main - prints the numbers from 1 to 100, followed by a new line
  *        but for multiples of three print Fizz instead of the number
  *        and for the multiples of five print Buzz.
- *        For numbers which are multiples of both three and five print FizzBuzz.
+ *	 For numbers which are multiples of both
  *
  * Return: Always 0.
  */
