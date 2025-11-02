098d66d7ee107ab91fff8a2e20ddec5ef5f03f42
Melissa <melissasad201@gmail.com>
2025-11-02 11:34:14 +0000

    push

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -7,9 +8,22 @@
  */
 int main(void)
 {
-    char *str;
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
     return (0);
 }
+julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strcmp.c -o 3-strcmp
+julien@ubuntu:~/$ ./3-strcmp 
+-15
+15
+0
+julien@ubuntu:~/$ 
+Repo:
+
+GitHub repository: holbertonschool-low_level_programming
+Directory: pointers_arrays_strings
+File: 3-strcmp.c
