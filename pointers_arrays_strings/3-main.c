a90558c4faa745eb3d191f4298c23698d50c71b9
Melissa <melissasad201@gmail.com>
2025-11-03 21:45:24 +0000

    push

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -8,22 +8,11 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
+    n = _strspn(s, f);
+    printf("%u\n", n);
     return (0);
 }
-julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strcmp.c -o 3-strcmp
-julien@ubuntu:~/$ ./3-strcmp 
--15
-15
-0
-julien@ubuntu:~/$ 
-Repo:
-
-GitHub repository: holbertonschool-low_level_programming
-Directory: pointers_arrays_strings
-File: 3-strcmp.c
