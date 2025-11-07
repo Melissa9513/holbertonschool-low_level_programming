b41137d504100f2cae8f3243edf3f2db55b6f300
Melissa <melissasad201@gmail.com>
2025-11-07 19:19:53 +0000

    push

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,20 +2,21 @@
 
 /**
  * _memset - fills memory with a constant byte
- * @s: pointer to the memory area to fill
- * @b: the constant byte to fill with
+ * @s: pointer to the memory area
+ * @b: constant byte to fill
  * @n: number of bytes to fill
  *
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i = 0;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
 
-    return (s);
+	return (s);
 }
