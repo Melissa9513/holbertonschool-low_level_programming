ba7bd60102c64aeb5e2c6ef1ec0cc157df5d5d88
Melissa <melissasad201@gmail.com>
2025-11-12 13:11:13 +0000

    push

--- malloc_free/0-malloc_checked.c
+++ malloc_free/0-malloc_checked.c
@@ -5,15 +5,15 @@
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
- * Return: pointer to allocated memory
- * If malloc fails, terminate with status 98
+ * Return: pointer to the allocated memory
+ *         exits with status 98 if malloc fails
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = malloc(b);
-    if (ptr == NULL)
-        exit(98);
-    return (ptr);
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
